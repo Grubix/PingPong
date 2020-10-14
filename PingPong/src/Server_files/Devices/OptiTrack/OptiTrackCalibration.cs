8ca7619403431f834dcb37e3f5a1201810161f29
Arentalon <bartekbabiaczyk@wp.pl>
2020-10-15 00:09:44 +0200

    Gotowa klasa do wykonywania kalibracji

--- PingPong/src/Server_files/Devices/OptiTrack/OptiTrackCalibration.cs
+++ PingPong/src/Server_files/Devices/OptiTrack/OptiTrackCalibration.cs
@@ -3,10 +3,8 @@ using MathNet.Numerics.LinearAlgebra;
 using System.Collections.Generic;
 using PingPong.Devices.KUKA;
 
-namespace PingPong.Devices.OptiTrack
-{
-    class OptiTrackCalibration
-    {
+namespace PingPong.Devices.OptiTrack {
+    class OptiTrackCalibration {
         private Vector<double> centroidKuka;
         private Vector<double> centroidOptiTrack;
         private Vector<double> trans;
@@ -14,15 +12,15 @@ namespace PingPong.Devices.OptiTrack
         private Matrix<double> matrixH;
         private Matrix<double> matrixR;
 
-        private List<Vector<double>> vectorsKuka;
-        private List<Vector<double>> vectorsOptiTrack;
+        private List<Vector<double>> vectorsFromKuka;
+        private List<Vector<double>> vectorsFromOptiTrack;
 
         private E6POS currentPositionKuka;
         private E6POS currentPositionOptiTrack;
 
         public OptiTrackCalibration(E6POS currentPositionKuka, E6POS currentPositionOptiTrack) {
-            vectorsKuka = new List<Vector<double>>();
-            vectorsOptiTrack = new List<Vector<double>>();
+            vectorsFromKuka = new List<Vector<double>>();
+            vectorsFromOptiTrack = new List<Vector<double>>();
 
             this.currentPositionKuka = currentPositionKuka;
             this.currentPositionOptiTrack = currentPositionOptiTrack;
@@ -42,10 +40,10 @@ namespace PingPong.Devices.OptiTrack
             //UseDefaultCalibration();
         }
 
-        public void AddCalibrationPoints() {
+        public void addCalibrationPoints() {
             Vector<double> vectorPointKuka = Vector<double>.Build.Dense(3);
             vectorPointKuka = currentPositionKuka.XYZ;
-            vectorsKuka.Add(vectorPointKuka);
+            vectorsFromKuka.Add(vectorPointKuka);
 
             Vector<double> positionPointOptiTrack = Vector<double>.Build.Dense(3);
             positionPointOptiTrack.Clear();
@@ -54,10 +52,77 @@ namespace PingPong.Devices.OptiTrack
                 positionPointOptiTrack[0] += currentPositionOptiTrack.X; //TODO: 
                 positionPointOptiTrack[1] += currentPositionOptiTrack.Y;
                 positionPointOptiTrack[2] += currentPositionOptiTrack.Z;
-                Thread.Sleep(5); // --> mozna chyba zastąpić poprzez funkcje lock() / Thread.Sleep(5) --> zatrzymuje główny wątek na 5 sek
+                //Thread.Sleep(5);  
+                //TODO --> mozna chyba zastąpić poprzez funkcje lock() / Thread.Sleep(5) --> zatrzymuje główny wątek na 5 sek
             }
             positionPointOptiTrack = positionPointOptiTrack / 200;
-            vectorsOptiTrack.Add(positionPointOptiTrack);
+            vectorsFromOptiTrack.Add(positionPointOptiTrack);
+        }
+
+        public void calculateCentroids() {
+            centroidKuka.Clear();
+            centroidOptiTrack.Clear();
+
+            foreach (Vector<double> vector in vectorsFromKuka) {
+                centroidKuka += vector;
+            }
+
+            foreach (Vector<double> vector in vectorsFromOptiTrack) {
+                centroidOptiTrack += vector;
+            }
+
+            centroidKuka = centroidKuka.Divide(vectorsFromKuka.Count);
+            centroidOptiTrack = centroidOptiTrack.Divide(vectorsFromOptiTrack.Count);
+
+        }
+
+        public void calculateMatrixH() {
+            Matrix<double> matrixPointOptiTrackk = Matrix<double>.Build.Dense(3, 1);
+            Matrix<double> matrixPointKuka = Matrix<double>.Build.Dense(1, 3);
+            Vector<double> pointOptiTrack = Vector<double>.Build.Dense(3);
+            Vector<double> pointKuka = Vector<double>.Build.Dense(3);
+            matrixPointOptiTrackk.Clear();
+            matrixPointKuka.Clear();
+            pointOptiTrack.Clear();
+            pointKuka.Clear();
+            matrixH.Clear();
+
+            for (int i = 0; i < vectorsFromKuka.Count; i++) {
+                pointOptiTrack = vectorsFromOptiTrack[i] - centroidOptiTrack;
+                pointKuka = vectorsFromKuka[i] - centroidKuka;
+
+                for (int j = 0; j < 3; j++) {
+                    matrixPointOptiTrackk[j, 0] = pointOptiTrack[j];
+                    matrixPointKuka[0, j] = pointKuka[j];
+                }
+
+                matrixH += (matrixPointOptiTrackk * matrixPointKuka);
+            }
+        }
+
+        public void calculateRotationAndTranslation() { 
+            // Rozklad SVD --> matrixH = U*W*VT 
+            var SVD = matrixH.Svd(true); // tworzy rozklad wedlug wartosci osobistych macierzy matrixH
+
+            Console.WriteLine(SVD.W); // SVD.W --> wyciaga macierz diagonalna --> posiada nieujemne wartosci osobliwe macierzy matrixH
+            matrixR = SVD.VT.Transpose() * SVD.U.Transpose(); // wymnażamy otrzymane macierze VT oraz U, sa to macierze ortogonalne (U^-1=U^T)
+
+            if (matrixR.Determinant() < 0) {
+                matrixR[0, 2] *= -1;
+                matrixR[1, 2] *= -1;
+                matrixR[2, 2] *= -1;
+            }
+
+            trans = -1 * matrixR * centroidOptiTrack + centroidKuka;
+
+        }
+
+        public void calibration() {
+            addCalibrationPoints();
+            calculateCentroids();
+            calculateMatrixH();
+            calculateRotationAndTranslation();
         }
     }
 }
+
